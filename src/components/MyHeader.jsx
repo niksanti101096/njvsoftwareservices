a82a1db6c9513c0aee452454fbc264ad824d50fe
NikzZz <santillan.nj.work@gmail.com>
2023-07-26 23:28:36 +0800

    Fixed header

--- src/components/MyHeader.jsx
+++ src/components/MyHeader.jsx
@@ -1,76 +1,62 @@
-import React from "react";
+import React, { useState } from "react";
+import CloseIcon from "@mui/icons-material/Close";
+import MenuIcon from "@mui/icons-material/Menu";
 
 function MyHeader() {
+  const [isNavOpen, setIsNavOpen] = useState(false);
+
   return (
-    <div>
+    <>
       <nav
-        className="navbar navbar-expand-lg bg-primary px-5 fixed-top"
+        className="navbar navbar-expand-lg bg-primary px-5 fixed-top d-flex justify-content-between"
         data-bs-theme="dark"
       >
-        <div className="container-fluid">
-          <a className="navbar-brand" href="/">
-            NJV Software Services
-          </a>
-          <div className="d-flex">
-            <button
-              className="navbar-toggler"
-              type="button"
-              data-bs-toggle="collapse"
-              data-bs-target="#navbarToggle"
-              aria-controls="navbarToggle"
-              aria-expanded="false"
-              aria-label="Toggle navigation"
-            >
-              <span className="navbar-toggler-icon"></span>
-            </button>
-            <div className="collapse navbar-collapse" id="navbarToggle">
-              <ul className="navbar-nav me-auto">
-                <li className="nav-item">
-                  <a
-                    className="nav-link hover-underline-animation"
-                    href="/aboutUs"
-                  >
-                    About Us
-                  </a>
-                </li>
-                <li className="nav-item">
-                  <a
-                    className="nav-link hover-underline-animation"
-                    href="/services"
-                  >
-                    Services
-                  </a>
-                </li>
-                <li className="nav-item">
-                  <a
-                    className="nav-link hover-underline-animation"
-                    href="/careers"
-                  >
-                    Careers
-                  </a>
-                </li>
-                <li className="nav-item">
-                  <a
-                    className="nav-link hover-underline-animation"
-                    href="/technologies"
-                  >
-                    Technologies
-                  </a>
-                </li>
-                <li className="nav-item">
-                  <a
-                    className="nav-link hover-underline-animation"
-                    href="/contactUs"
-                  >
-                    Contact Us
-                  </a>
-                </li>
-              </ul>
-            </div>
-          </div>
+        <a href="/" className="navbar-brand">
+          NJV Software Services
+        </a>
+        <div>
+          <ul className={isNavOpen? "#navigation active navbar-nav" : "#navigation navbar-nav"} id="navigation">
+            <li className="nav-item">
+              <a className="nav-link hover-underline-animation" href="/aboutUs">
+                About Us
+              </a>
+            </li>
+            <li className="nav-item">
+              <a
+                className="nav-link hover-underline-animation"
+                href="/services"
+              >
+                Services
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link hover-underline-animation" href="/careers">
+                Careers
+              </a>
+            </li>
+            <li className="nav-item">
+              <a
+                className="nav-link hover-underline-animation"
+                href="/technologies"
+              >
+                Technologies
+              </a>
+            </li>
+            <li className="nav-item">
+              <a
+                className="nav-link hover-underline-animation"
+                href="/contactUs"
+              >
+                Contact Us
+              </a>
+            </li>
+          </ul>
+        </div>
+        <div type="button" className="text-dark" id="mobile">
+          <i id="bar" onClick={() => setIsNavOpen(!isNavOpen)}>{isNavOpen ? <CloseIcon /> : <MenuIcon />}</i>
         </div>
       </nav>
-    </div>
+    </>
   );
 }
 
