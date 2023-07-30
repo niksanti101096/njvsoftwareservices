2a26c253f84e868086eacf577f00aedee4fa31e6
John Harvey Castro <jharveycastro2@gmail.com>
2023-07-30 08:04:59 +0800

    added something here so that I can push

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
+import "./Career.css";
 import MyHeader from "./components/MyHeader";
 import Home from "./pages/Home";
 import About from "./pages/About";
