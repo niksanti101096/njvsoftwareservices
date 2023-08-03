91d63683f828e3de6c11dae2c210ce12f98d2a4f
NikzZz <santillan.nj.work@gmail.com>
2023-08-03 09:22:05 +0800

    WIP

--- src/components/MyFooter.jsx
+++ src/components/MyFooter.jsx
@@ -3,8 +3,8 @@ import React from "react";
 function MyFooter() {
   return (
     <div className="footer bg-primary py-3">
-      <div className="d-flex justify-content-center align-items-center">
-        <p className=" mb-0">Copyrights © 2023 NJV Software Services, All Rights Reserved</p>
+      <div className="d-flex justify-content-center align-items-center text-light">
+        <p className=" mb-0">Copyrights © 2023 NJV Software Services, All Rights Reserved | This website is still in work in progress.</p>
       </div>
     </div>
   );
